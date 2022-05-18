ec134ddf5039369b53e3956cd69798ba50e6a28b
Jessenia Lorenzo <remote.jessy@gmail.com>
2022-05-17 21:07:10 -0400

    Changes.
    Changes.

---
+++ R/reading-in-data.R
@@ -0,0 +1,10 @@
+setwd('C:/Users/jesse/Documents/Github/Jessenia_Lorenzo_WGU_MSDA/R')
+
+install.packages('tidyverse')
+
+library('tidyverse')
+
+df <- read_csv('Boston_housing_data.csv')
+
+head(df,5)
+
